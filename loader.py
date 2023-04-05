d78f3714228f97468e835e3bc0e6473309384018
lumino33 <ledinhhung332000@gmail.com>
2023-04-05 14:18:18 +0700

    active learning

---
+++ loader.py
@@ -0,0 +1,99 @@
+import glob
+import os
+from PIL import Image, ImageFilter
+
+from torch.utils.data import Dataset, DataLoader
+import torch
+import torchvision.transforms as transforms
+import numpy as np
+import random
+import cv2
+
+class RotationLoader(Dataset):
+    def __init__(self, is_train=True, transform=None, path='./DATA'):
+        self.is_train = is_train
+        self.transform = transform
+        # self.h_flip = transforms.RandomHorizontalFlip(p=1)
+        if self.is_train: # train
+            self.img_path = glob.glob('./DATA/train/*')
+        else:
+            self.img_path = glob.glob('./DATA/test/*')
+        
+
+    def __len__(self):
+        return len(self.img_path)
+
+    def __getitem__(self, idx):
+        img = cv2.imread(self.img_path[idx])
+        img = Image.fromarray(img)
+
+        if self.is_train:
+            img = self.transform(img)
+            img1 = torch.rot90(img, 1, [1,2])
+            img2 = torch.rot90(img, 2, [1,2])
+            img3 = torch.rot90(img, 3, [1,2])
+            imgs = [img, img1, img2, img3]
+            rotations = [0,1,2,3]
+            random.shuffle(rotations)
+            # print(img.shape, img1.shape, img2.shape, img3.shape)
+            return imgs[rotations[0]], imgs[rotations[1]], imgs[rotations[2]], imgs[rotations[3]], rotations[0], rotations[1], rotations[2], rotations[3], self.img_path[idx]
+        else:
+            img = self.transform(img)
+            img1 = torch.rot90(img, 1, [1,2])
+            img2 = torch.rot90(img, 2, [1,2])
+            img3 = torch.rot90(img, 3, [1,2])
+            imgs = [img, img1, img2, img3]
+            rotations = [0,1,2,3]
+            random.shuffle(rotations)
+            return imgs[rotations[0]], imgs[rotations[1]], imgs[rotations[2]], imgs[rotations[3]], rotations[0], rotations[1], rotations[2], rotations[3], self.img_path[idx]
+
+class Loader2(Dataset):
+    def __init__(self, is_train=True, transform=None, path='./DATA', path_list=None):
+        self.is_train = is_train
+        self.transform = transform
+        self.path_list = path_list
+
+        if self.is_train: # train
+            self.img_path = path_list
+        else:
+            if path_list is None:
+                self.img_path = glob.glob('./DATA/train/*/*') # for loss extraction
+            else:
+                self.img_path = path_list
+    def __len__(self):
+        return len(self.img_path)
+
+    def __getitem__(self, idx):
+        if self.is_train:
+            img = cv2.imread(self.img_path[idx][:-1])
+        else:
+            if self.path_list is None:
+                img = cv2.imread(self.img_path[idx])
+            else:
+                img = cv2.imread(self.img_path[idx][:-1])
+        img = Image.fromarray(img)
+        img = self.transform(img)
+        label = int(self.img_path[idx].split('/')[-2])
+
+        return img, label
+
+class Loader(Dataset):
+    def __init__(self, is_train=True, transform=None, path='./DATA'):
+        self.classes = 10 
+        self.is_train = is_train
+        self.transform = transform
+        if self.is_train: # train
+            self.img_path = glob.glob('./DATA/train/*/*')
+        else:
+            self.img_path = glob.glob('./DATA/test/*/*')
+
+    def __len__(self):
+        return len(self.img_path)
+
+    def __getitem__(self, idx):
+        img = cv2.imread(self.img_path[idx])
+        img = Image.fromarray(img)
+        img = self.transform(img)
+        label = int(self.img_path[idx].split('/')[-2])
+
+        return img, label
---
+++ make_batches.py
@@ -0,0 +1,113 @@
+'''Train CIFAR10 with PyTorch.'''
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+
+import torchvision
+import torchvision.transforms as transforms
+
+import os
+import argparse
+import random
+import numpy as np
+
+from models import *
+from loader import Loader, RotationLoader
+from utils import progress_bar
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+best_acc = 0  # best test accuracy
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+
+
+transform_test = transforms.Compose([
+    transforms.Resize(512),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+trainset = RotationLoader(is_train=True,  transform=transform_test)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=1, shuffle=False, num_workers=2)
+
+net = ResNet18()
+# net.linear = nn.Linear(512, 4)
+net = net.to(device)
+
+if device == 'cuda':
+    net = torch.nn.DataParallel(net)
+    cudnn.benchmark = True
+
+checkpoint = torch.load('checkpoint/rotation_100.0.pth')
+net.load_state_dict(checkpoint['net'])
+
+criterion = nn.CrossEntropyLoss()
+
+def test(epoch):
+    global best_acc
+    net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (inputs, inputs1, inputs2, inputs3, targets, targets1, targets2, targets3, path) in enumerate(trainloader):
+            inputs, inputs1, targets, targets1 = inputs.to(device), inputs1.to(device), targets.to(device), targets1.to(device)
+            inputs2, inputs3, targets2, targets3 = inputs2.to(device), inputs3.to(device), targets2.to(device), targets3.to(device)
+            outputs = net(inputs)
+            outputs1 = net(inputs1)
+            outputs2 = net(inputs2)
+            outputs3 = net(inputs3)
+            loss1 = criterion(outputs, targets)
+            loss2 = criterion(outputs1, targets1)
+            loss3 = criterion(outputs2, targets2)
+            loss4 = criterion(outputs3, targets3)
+            loss = (loss1+loss2+loss3+loss4)/4.
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+            loss = loss.item()
+            s = str(float(loss)) + '_' + str(path[0]) + "\n"
+
+            with open('./rotation_loss.txt', 'a') as f:
+                f.write(s)
+            progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                         % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+if __name__ == "__main__":
+    # test(1)
+    with open('./rotation_loss.txt', 'r') as f:
+        losses = f.readlines()
+
+    loss_1 = []
+    name_2 = []
+
+    for j in losses:
+        loss_1.append(j[:-1].split('_')[0])
+        name_2.append(j[:-1].split('_')[1])
+
+    s = np.array(loss_1)
+    sort_index = np.argsort(s)
+    x = sort_index.tolist()
+    x.reverse()
+    print(len(x))
+    sort_index = np.array(x) # convert to high loss first
+
+    if not os.path.isdir('loss'):
+        os.mkdir('loss')
+    for i in range(10):
+        # sample minibatch from unlabeled pool 
+        sample5000 = sort_index[i*356:(i+1)*356]
+        # sample1000 = sample5000[[j*5 for j in range(1000)]]
+        # b = np.zeros(10)
+        # for jj in sample5000:
+        #     b[int(name_2[jj].split('/')[-2])] +=1
+        # print(f'{i} Class Distribution: {b}')
+        s = './loss/batch_' + str(i) + '.txt'
+        root = '/home/hungld11/Intern/khanghn1/PT4AL'
+        print(root)
+        for k in sample5000:
+            with open(s, 'a') as f:
+                f.write(name_2[k].split('/')[3]+'\n')
